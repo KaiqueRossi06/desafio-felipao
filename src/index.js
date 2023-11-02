10c59cab36def909860eec26f9746b313c75a3d7
KaiqueRossi06 <kaiquerossi620@gmail.com>
2023-11-01 21:03:08 -0300

    desafio felipao

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-let nome = "jorgin";
-console.log(nome);
+
+//entrada
+const nomeHeroi = ("Super Geraldinho")
+let heroiXp = 1
+let rankHeroi = ""
+//...
+
+if(heroiXp > 0 && heroiXp <= 1000){
+    rankHeroi = ("Ferro.")
+}
+ if(heroiXp > 1000 && heroiXp<=2000){
+    rankHeroi = ("Bronze.")
+}else if(heroiXp > 2000 && heroiXp<=5000){
+    rankHeroi = ("Prata.")
+}else if(heroiXp > 5000 && heroiXp<=7000){
+    rankHeroi = ("Ouro.")
+}else if(heroiXp > 7000 && heroiXp<=8000){
+    rankHeroi = ("Platina.")
+}else if(heroiXp > 8000 && heroiXp<=9000){
+    rankHeroi = ("Ascendente.")
+}else if(heroiXp > 9000 && heroiXp<=10000){
+    rankHeroi = ("Imortal.")
+}else if(heroiXp > 10000){
+    rankHeroi = ("Radiante")
+}
+//console
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + rankHeroi)
